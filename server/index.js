853cfe3bb7518e25e9871517aed1c809fb246634
piyush77 <piyushrahangdale21@gmail.com>
2023-06-19 13:24:02 +0530

    added link in server

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://pdfstore.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
