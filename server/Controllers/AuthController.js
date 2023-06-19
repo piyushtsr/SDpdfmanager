3acd161250cb937c1b8c5ffc6d37e182edcc6681
piyush77 <piyushrahangdale21@gmail.com>
2023-06-19 14:06:08 +0530

    cookie probb

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -15,6 +15,7 @@ module.exports.Signup = async (req, res, next) => {
     res.cookie("token", token, {
       withCredentials: true,
       httpOnly: true,
+      secure: true
     });
     res
       .status(201)
@@ -44,6 +45,7 @@ module.exports.Login = async (req, res, next) => {
        res.cookie("token", token, {
          withCredentials: true,
          httpOnly: true,
+         secure: true
        });
        res.status(201).json({ message: "User logged in successfully", success: true });
        next()
