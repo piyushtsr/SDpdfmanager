75685f8afec6b17b8a4b74bfdce71f87db5322e0
piyush77 <piyushrahangdale21@gmail.com>
2023-06-19 16:02:20 +0530

    cookie issue2

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["https://pdfstore.onrender.com"],
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
